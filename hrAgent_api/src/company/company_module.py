9bd4964d81781257b8778b14f59ef66ad01888ad
James Eubanks <jeubanks.github@gmail.com>
2023-12-27 21:15:10 -0500

    adding company module and fastapi title with index redirect

---
+++ hrAgent_api/src/company/company_module.py
@@ -0,0 +1,12 @@
+from src.company.company_service import CompanyService
+from src.company.company_controller import CompanyController
+
+
+class CompanyModule:
+
+    def __init__(self):
+        self.providers = [CompanyService]
+        self.controllers = [CompanyController]
+
+
+

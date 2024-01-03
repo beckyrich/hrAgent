6c5452424b2f3679b4d63dc203a8acd2aa530e63
Becky Richardson <rebecca.s.richardson4@gmail.com>
2024-01-03 17:47:06 -0600

    updated company_entity table name to company

--- hrAgent_api/src/company/company_entity.py
+++ hrAgent_api/src/company/company_entity.py
@@ -3,18 +3,18 @@ from sqlalchemy import Column, Integer, String, Float, BigInteger, Numeric, Time
     
     
 class Company(config.Base):
-    __tablename__ = "master_company"
+    __tablename__ = "company"
     
-    master_company_id = Column(BigInteger, primary_key=True, autoincrement=True)
-    co_name = Column(String, unique=True)
+    company_id = Column(BigInteger, primary_key=True, autoincrement=True)
+    parent_co_id = Column(BigInteger)
     address_line_1 = Column(String)
     address_line_2 = Column(String)
     postal_code = Column(Numeric)
     state_id = Column(SmallInteger)
     created = Column(TimeStamp)
     last_updated = Column(TimeStamp)
-    company_type = Column(Integer)
-    status_id = Column(Integer)
+    point_of_contact = Column(String)
+    poc_email = Column(String)
 
         
     

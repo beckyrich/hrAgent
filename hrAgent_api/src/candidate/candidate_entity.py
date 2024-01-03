d72a5569d4ac113ce83f5b49e63a11aee932fb18
Becky Richardson <rebecca.s.richardson4@gmail.com>
2024-01-03 17:20:54 -0600

    updated candidate_entity columns

--- hrAgent_api/src/candidate/candidate_entity.py
+++ hrAgent_api/src/candidate/candidate_entity.py
@@ -1,11 +1,20 @@
 from orm_config import config
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy import Column, Integer, String, Float, BigInteger, Numeric, Date, SmallInteger, TimeStamp
     
     
 class Candidate(config.Base):
     __tablename__ = "candidate"
     
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String, unique=True)
+    candidate_id = Column(BigInteger, primary_key=True, autoincrement=True)
+    candidate_first_name = Column(String)
+    candidate_last_name = Column(String)
+    candidate_email = Column(String, unique=True)
+    candidate_phone = Column(Numeric)
+    candidate_birthdate = Column(Date)
+    candidate_gender_id = Column(SmallInteger)
+    created = Column(TimeStamp)
+    last_updated = Column(TimeStamp)
+    last_login = Column(TimeStamp)
+    status_id = Column(Integer)
         
     

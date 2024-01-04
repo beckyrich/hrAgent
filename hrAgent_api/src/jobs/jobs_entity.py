6f6b955973e9b1a41a81b305c3f5adc8f7796d59
Becky Richardson <rebecca.s.richardson4@gmail.com>
2024-01-03 18:08:50 -0600

    attempted to add job type and level enums

--- hrAgent_api/src/jobs/jobs_entity.py
+++ hrAgent_api/src/jobs/jobs_entity.py
@@ -1,14 +1,30 @@
+import enum
 from orm_config import config
 from sqlalchemy import Column, Integer, String, Float, Enum, Boolean
     
-    
+class JobType(enum.Enum):
+    seasonal = 1
+    fte = 2
+    part_time = 3
+    1099 = 4
+    agency = 5
+    contract_to_hire = 6
+
+class JobLevel(enum.Enum):
+    entry = 1
+    mid = 2
+    senior = 3
+    principal = 4
+    management = 5
+    executive = 6    
+
 class Jobs(config.Base):
     __tablename__ = "jobs"
     
     job_id = Column(BigInteger, primary_key=True, autoincrement=True)
-    job_type = Column(Enum)
+    job_type = Column(Enum(JobType))
     job_title = Column(String)
-    job_level = Column(Enum)
+    job_level = Column(Enum(JobLevel))
     remote = Column(Boolean)
     created = Column(TimeStamp)
     last_updated = Column(TimeStamp)

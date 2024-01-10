4acf37ceb3d93a668031d6137d1763f929b424c9
Becky Richardson <rebecca.s.richardson4@gmail.com>
2024-01-10 14:34:59 -0600

    updated jobs_model

--- hrAgent_api/src/jobs/jobs_model.py
+++ hrAgent_api/src/jobs/jobs_model.py
@@ -1,6 +1,13 @@
+from datetime import datetime
 from pydantic import BaseModel
     
     
 class Jobs(BaseModel):
     job_id: str
+    job_type:
+    job_title: str
+    jobs_level: 
+    remote: bool
+    created: datetime
+    last_updated: datetime
     

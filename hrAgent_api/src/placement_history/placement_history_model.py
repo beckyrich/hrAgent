96082e4b2d5b4d8e86f314beabeec1fc82b68df8
Becky Richardson <rebecca.s.richardson4@gmail.com>
2024-01-09 18:52:08 -0600

    updated placement_history_model

--- hrAgent_api/src/placement_history/placement_history_model.py
+++ hrAgent_api/src/placement_history/placement_history_model.py
@@ -1,6 +1,16 @@
+import datetime from datetime
 from pydantic import BaseModel
     
     
 class PlacementHistory(BaseModel):
-    name: str
-    
+    placement_history_id: int
+    job_id: int
+    company_id: int
+    candidate_id: int
+    start_date: datetime
+    end_date: datetime
+    created: datetime
+    last_updated: datetime
+    hiring_manager_id: int
+    hourly_bill_rate: float
+    hourly_pay_rate: float

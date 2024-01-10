db4c60d998176be496ddc99c219b1b0c9d880793
Becky Richardson <rebecca.s.richardson4@gmail.com>
2024-01-09 18:25:52 -0600

    updated status_id

--- hrAgent_api/src/candidate/candidate_model.py
+++ hrAgent_api/src/candidate/candidate_model.py
@@ -9,4 +9,9 @@ class Candidate(BaseModel):
     candidate_email: str
     candidate_phone: int
     candidate_birthdate: datetime
+    candidate_gender_id: int
+    created: datetime
+    last_updated: datetime
+    last_login: datetime
+    status_id: int
     

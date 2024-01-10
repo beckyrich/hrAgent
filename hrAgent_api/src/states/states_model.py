20e3499dcade7f80ee1d2ff320b2cf35dd1c88db
Becky Richardson <rebecca.s.richardson4@gmail.com>
2024-01-09 18:55:23 -0600

    updated states_model

--- hrAgent_api/src/states/states_model.py
+++ hrAgent_api/src/states/states_model.py
@@ -1,6 +1,11 @@
+from datetime import datetime
 from pydantic import BaseModel
     
     
 class States(BaseModel):
-    name: str
-    
+    state_id: int
+    state_abbv: str
+    state_name: str
+    state_capital: str
+    created: datetime
+    last_updated: datetime

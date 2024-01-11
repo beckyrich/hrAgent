84c5e8b80090262c31e74d998d51b94d761a7e9e
Becky Richardson <rebecca.s.richardson4@gmail.com>
2024-01-10 20:32:40 -0600

    moved enums to new file

--- hrAgent_api/src/jobs/jobs_entity.py
+++ hrAgent_api/src/jobs/jobs_entity.py
@@ -1,30 +1,14 @@
-import enum
+from src.jobs.jobs_enums import JobTypeEnum, JobLevelEnum
 from orm_config import config
-from sqlalchemy import Column, Integer, String, Float, Enum, Boolean, DateTime, BigInteger
-    
-class JobType(enum.Enum):
-    seasonal = 'seasonal'
-    fte = 'fte'
-    part_time = 'part_time'
-    ten_99 = 'ten_99'
-    agency = 'agency'
-    contract_to_hire = 'contract_to_hire'
-
-class JobLevel(enum.Enum):
-    entry = 'entry'
-    mid = 'mid'
-    senior = 'senior'
-    principal = 'principal'
-    management = 'managment'
-    executive = 'executive'    
+from sqlalchemy import Column, Integer, String, Float, Enum, Boolean, DateTime, BigInteger   
 
 class Jobs(config.Base):
     __tablename__ = "jobs"
     
     job_id = Column(BigInteger, primary_key=True, autoincrement=True)
-    job_type = Column(Enum(JobType))
+    job_type = Column(Enum(JobTypeEnum))
     job_title = Column(String)
-    job_level = Column(Enum(JobLevel))
+    job_level = Column(Enum(JobLevelEnum))
     remote = Column(Boolean)
     created = Column(DateTime)
     last_updated = Column(DateTime)
--- hrAgent_api/src/jobs/jobs_model.py
+++ hrAgent_api/src/jobs/jobs_model.py
@@ -1,22 +1,6 @@
+from src.jobs.jobs_enums import JobTypeEnum, JobLevelEnum
 from datetime import datetime
-from enum import Enum
 from pydantic import BaseModel
-
-class JobTypeEnum(str, Enum):
-    seasonal = 'seasonal'
-    fte = 'fte'
-    part_time = 'part_time'
-    ten_99 = 'ten_99'
-    agency = 'agency'
-    contract_to_hire = 'contract_to_hire'
-
-class JobLevelEnum(str, Enum):
-    entry = 'entry'
-    mid = 'mid'
-    senior = 'senior'
-    principal = 'principal'
-    management = 'management'
-    executive = 'executive'
     
 class Jobs(BaseModel):
     job_id: str

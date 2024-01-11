01a8c02bbc3621322e616c77ca0c9cd1a5897953
Becky Richardson <rebecca.s.richardson4@gmail.com>
2024-01-10 18:54:32 -0600

    added not null to master company entity file

--- hrAgent_api/src/master_company/master_company_entity.py
+++ hrAgent_api/src/master_company/master_company_entity.py
@@ -6,7 +6,7 @@ class MasterCompany(config.Base):
     __tablename__ = "master_company"
     
     master_company_id = Column(BigInteger, primary_key=True, autoincrement=True)
-    co_name = Column(String, unique=True)
+    co_name = Column(String, unique=True, nullable=False)
     address_line_1 = Column(String)
     address_line_2 = Column(String)
     postal_code = Column(String)

2feabaf392c12488ecedfad90f07992feb7897a4
Becky Richardson <rebecca.s.richardson4@gmail.com>
2024-01-10 18:58:27 -0600

    state_name updated to not null in entity file

--- hrAgent_api/src/states/states_entity.py
+++ hrAgent_api/src/states/states_entity.py
@@ -7,7 +7,7 @@ class States(config.Base):
     
     state_id = Column(SmallInteger, primary_key=True, autoincrement=True)
     state_abbv = Column(String)
-    state_name = Column(String)
+    state_name = Column(String, nullable=False)
     state_capital = Column(String)
     created = Column(DateTime)
     last_updated = Column(DateTime)

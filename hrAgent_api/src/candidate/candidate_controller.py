ac0dba514c103137d7b331fedc7c4aa20539ea15
James Eubanks <james.eubanks@wowinc.com>
2024-04-25 09:04:06 -0400

    adding dependency

--- hrAgent_api/src/candidate/candidate_controller.py
+++ hrAgent_api/src/candidate/candidate_controller.py
@@ -1,5 +1,5 @@
 from nest.core import Controller, Get, Post, Depends
-
+from app import oauth2_scheme
 from src.candidate.candidate_service import CandidateService
 from src.candidate.candidate_model import Candidate
 
@@ -9,7 +9,7 @@ class CandidateController:
     service: CandidateService = Depends(CandidateService)
 
     @Get("/get_candidate")
-    def get_candidate(self):
+    def get_candidate(self, token = Depends(oauth2_scheme)):
         return self.service.get_candidate()
 
     @Post("/add_candidate")

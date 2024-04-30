9c1c713c2be7b2349f8d031f074b996d48499bc9
Becky Richardson <rebecca.s.richardson4@gmail.com>
2024-04-30 06:38:21 -0500

    check username and password Auth

--- hrAgent_api/src/auth/auth.py
+++ hrAgent_api/src/auth/auth.py
@@ -2,12 +2,31 @@
 # Made it through "Get current user" in FastAPI docs
 from typing import Annotated
 
-from nest.core import Depends
-from fastapi.security import OAuth2PasswordBearer
+from nest.core import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
 from fastapi import APIRouter
 
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "fakehashedsecret",
+        "disabled": False,
+    },
+    "alice": {
+        "username": "alice",
+        "full_name": "Alice Wonderson",
+        "email": "alice@example.com",
+        "hashed_password": "fakehashedsecret2",
+        "disabled": True,
+    },
+}
 app = APIRouter(prefix= "/auth")
+
+def fake_hash_password(password: str):
+    return "fakehashed" + password
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
@@ -25,6 +44,12 @@ def fake_decode_token(token):
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
     user = fake_decode_token(token)
     return user
+
+@app.post("/token")
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user_dict = fake_users_db.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
 
 @app.get("/users/me")
 async def read_users_me(current_user: Annotated[User, Depends(get_current_user)]):
